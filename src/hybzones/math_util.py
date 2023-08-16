65e7a4c794ca5e5992c85c84aaecd60c471c15b3
nwcol <nwcollier@wisc.edu>
2023-08-16 10:56:04 -0500

    Revert "oops"
    This reverts commit 560fd11e34eb8e40d85955b16a9ecc7bed2533ed.

---
+++ src/hybzones/math_util.py
@@ -0,0 +1,36 @@
+import math
+
+import numpy as np
+
+import scipy
+
+from scipy import stats
+
+from scipy.special import erfc
+
+import scipy.optimize as opt
+
+import time
+
+
+
+def compute_pd(x, s):
+    """Compute a vector of probability density values for a normal distribution
+    with standard deviation s, mean 0.
+    """
+    return 1 / (s * np.sqrt(2 * np.pi)) * np.exp(-0.5 * np.square(x / s))
+
+
+def logistic_fxn(x, k, x_0):
+    return 1 / (1.0 + np.exp(-k * (x - x_0)))
+
+
+def optimize_logistic(data, x):
+    popt, pcov = opt.curve_fit(logistic_fxn, x, data)
+    return popt, pcov
+
+
+def get_time_string():
+    return str(time.strftime("%H:%M:%S", time.localtime()))
+
+
